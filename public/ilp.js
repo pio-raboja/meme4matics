1c2ee74115200a43cfab51f63d8253e49420a734
Pio Raboja <pio.raboja@yandex.com>
2021-02-26 20:23:28 +0000

    formatting

--- public/ilp.js
+++ public/ilp.js
@@ -13,7 +13,7 @@ const feedbackOptions = ["You have structured your application well," +
 "Plugins in the pom.xml file were specified within a" +
 " <pluginManagement> block. As warned about on the discussion " +
 "forum, this was generally a bad idea for this coursework.",
-	"Your implementation was overengineered",
+	"Your implementation was overengineered.",
 	"Fuck you."
 ]
 
@@ -23,7 +23,7 @@ const score = randint(40, 58);
 const codeblock = document.querySelector("#ILPGrading");
 codeblock.innerText = `The raw overall mark is (${score}/75)\n\n` +
 	`The awarded mark includes a deduction for any days late \n` +
-	`(5% per calendar day).\n\nDays late: (0)\nThe awarded mark is (${score}/75)\n\n ` +
+	`(5% per calendar day).\n\nDays late: (0)\nThe awarded mark is (${score}/75)\n\n` +
 	`As described in the coursework instructions, marks are awarded for two aspects: ` +
 	`readability and correctness.  See below for comments on these ` +
 	`and other aspects of your submission.\n\n${feedback}`
